5578a6d2192da59cf5f489b8189d69fff1a8244a
MarinBotScript <marinfrank222@gmail.com>
2025-04-17 15:28:54 -0500

    creacion de las cards

--- src/Components/CustomCard/CustomCard.tsx
+++ src/Components/CustomCard/CustomCard.tsx
@@ -1,10 +1,60 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+interface Props {
+  image: string;
+  name: string;
+  race: string;
+  baseKi: string;
+  totalKi: string;
+  afilliation: string;
+}
 
 
-
-
-function CustomCard() {
+function CustomCard(
+  { image, name, race, baseKi, totalKi, afilliation }: Props
+  
+) {
   return (
-    <div>CustomCard</div>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia className='imageBackground'
+        component="img"
+        alt="green iguana"
+        height="140"
+        image= {image}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h2" component="div">
+          {name}
+        </Typography>
+        <Typography className='descriptionTextColor' variant="caption">
+          {race}
+        </Typography>
+        <Typography className='descriptionTextColorTitle' variant="h5">
+          Base kI:
+        </Typography>
+        <Typography className='descriptionTextColor' variant="caption">
+          {baseKi}
+        </Typography>
+        <Typography className='descriptionTextColorTitle' variant="h5">
+          Ttotal KI:
+        </Typography>
+        <Typography className='descriptionTextColor' variant="caption">
+          {totalKi}
+        </Typography>
+        <Typography className='descriptionTextColorTitle' variant="h5">
+          Afilliation:
+        </Typography>
+        <Typography className='descriptionTextColor' variant="caption">
+          {afilliation}
+        </Typography>
+      </CardContent>
+    </Card>
   )
 }
 
