61f8a1762159aafda3f57a4ab446bf2fe631aeff
thePanaMiguel09 <miguichavezbarreram@gmail.com>
2025-04-17 14:25:32 -0500

    sección main y logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import Header from "./Components/Header/Header";
 import "./App.css";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Header from "./Components/Header/Header";
+import logo from "/logo_dragonballapi.webp"
 
 const queryCliente = new QueryClient();
 
@@ -10,6 +12,10 @@ function App() {
     <QueryClientProvider client={queryCliente}>
       <div className="container">
         <Header />
+        <div className="logoContainer">
+          <img src={logo} alt="logoDragonBall" className="logoImage"/>
+          <h1 className="text-logo">The Dragon Ball API</h1>
+        </div>
       </div>
     </QueryClientProvider>
   );
