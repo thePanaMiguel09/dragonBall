84bab611af7ef2e09030692c890bc4d3e486f791
thePanaMiguel09 <miguichavezbarreram@gmail.com>
2025-04-20 14:58:25 -0500

    cambios card

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 import Home from "./pages/HomePage/Home";
 
-
 const queryCliente = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryCliente}>
       <div className="container">
-        
-        <Home/>
-
+        <Home />
       </div>
     </QueryClientProvider>
   );
