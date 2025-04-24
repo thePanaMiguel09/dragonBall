13924257c34513f74e6fa0a7059923876b718563
thePanaMiguel09 <miguichavezbarreram@gmail.com>
2025-04-23 21:38:56 -0500

    Loader indicator added

--- src/pages/DetailsPage/Details.tsx
+++ src/pages/DetailsPage/Details.tsx
@@ -2,42 +2,54 @@ import "./Details.css";
 import { useParams } from "react-router-dom";
 import { useCharacter } from "../../hooks/useCharacter";
 
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+
 function Details() {
   const { id } = useParams();
   const character = useCharacter(id!);
 
   return (
     <div className="detailsPage">
-      <div className="cardContainer">
-        <div className="imageContainer">
-          <img src={character.data?.image} alt="" className="characterImage" />
-        </div>
-        <div className="cardContentDetails">
-          <div className="headingCard">
-            <h2>{character.data?.name}</h2>
-            <p>{character.data?.originPlanet.name}</p>
+      {character.isLoading ? (
+        <Box sx={{ display: "flex" }}>
+          <CircularProgress color="warning"/>
+        </Box>
+      ) : (
+        <div className="cardContainer">
+          <div className="imageContainer">
+            <img
+              src={character.data?.image}
+              alt=""
+              className="characterImage"
+            />
           </div>
-          <div className="mainContentCard">
-            <div>
-              <h2>Race</h2>
-              <p>{character.data?.race}</p>
+          <div className="cardContentDetails">
+            <div className="headingCard">
+              <h2>{character.data?.name}</h2>
+              <p>{character.data?.originPlanet.name}</p>
             </div>
-            <div>
-              <h2>Gender</h2>
-              <p>{character.data?.gender}</p>
+            <div className="mainContentCard">
+              <div>
+                <h2>Race</h2>
+                <p>{character.data?.race}</p>
+              </div>
+              <div>
+                <h2>Gender</h2>
+                <p>{character.data?.gender}</p>
+              </div>
+              <div>
+                <h2>Status</h2>
+                <p>
+                  {character.data?.deletedAt !== null
+                    ? character.data?.deletedAt
+                    : "Alive"}
+                </p>
+              </div>
             </div>
-            <div>
-              <h2>Status</h2>
-              <p>
-                {character.data?.deletedAt !== null
-                  ? character.data?.deletedAt
-                  : "Alive"}
-              </p>
-            </div>
-           
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
