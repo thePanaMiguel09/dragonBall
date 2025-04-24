f5afa78468b727934f931bcad1aa241146f37360
MarinBotScript <marinfrank222@gmail.com>
2025-04-23 23:27:45 -0500

    Eliminacion de los importes no usados

--- src/Components/CustomCard/CustomCard.tsx
+++ src/Components/CustomCard/CustomCard.tsx
@@ -1,6 +1,4 @@
 import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Box from "@mui/material/Box";
 import "./CustomCard.css";
 import { CardActionArea } from "@mui/material";
 import { useNavigate } from "react-router-dom";
